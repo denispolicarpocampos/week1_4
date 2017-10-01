9cd286928db935f07f41ee8ad3ccc19172fb1198
Daniel Moreto <dfmoreto@gmail.com>
2017-10-01 17:23:47 -0300

    Locales was configured to read subdirectories to put each language in a folder

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Week14
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
   end
 end
