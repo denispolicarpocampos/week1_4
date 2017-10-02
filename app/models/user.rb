5d39b76176b3fa33c18a75423be941a86b391c33
Stefano Oliveira <oliveira.stefano@gmail.com>
2017-10-01 23:12:32 -0300

    correcao na declaracao do has_many, para uma forma clara de se entender.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-	has_many :likes
+	
+  has_many: likes_given, :class_name => 'Like'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
