65d509330492be5a94ac39d3ce56071e86e47483
osmarluz <osmar.luz@gmail.com>
2017-10-02 21:06:42 -0300

    Create relationships between users through connections table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :tweets
+  has_many :connections
+  has_many :followees, through: :connections
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
