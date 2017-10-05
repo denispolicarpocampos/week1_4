7be85becca4847f98c901165655f935bc4cc4962
Daniel Moreto <dfmoreto@gmail.com>
2017-10-05 17:04:02 -0300

    Created a module to login for Feature specs

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -9,9 +9,22 @@ module ControllerMacros
   end
 end
 
+
+module FeatureMacros
+  def login_user
+    let(:login_user) { create(:user) }
+    background(:each) do
+      login_as login_user
+    end
+  end
+end
+
+
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
+  config.extend FeatureMacros, type: :feature
 end
 
 
