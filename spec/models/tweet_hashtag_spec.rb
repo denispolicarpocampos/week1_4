21e8ba3ad0f15c84852c0081e876bf203e349e28
osmarluz <osmar.luz@gmail.com>
2017-10-04 22:57:20 -0300

    Fix syntax error on tweet_hashtag_spec.rb

--- spec/models/tweet_hashtag_spec.rb
+++ spec/models/tweet_hashtag_spec.rb
@@ -2,5 +2,5 @@ require "rails_helper"
 
 RSpec.describe Connection do
   it { is_expected.to belong_to(:tweet) }
-  it { is_expected.to belong_to(:hashtag }
+  it { is_expected.to belong_to(:hashtag) }
 end
