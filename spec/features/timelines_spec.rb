895e572758426a68c3a168107af01367db1fc2b5
Denis Campos <campos_bra@hotmail.com>
2017-10-05 21:14:16 -0300

    Update integration test of timelines page

--- spec/features/timelines_spec.rb
+++ spec/features/timelines_spec.rb
@@ -3,17 +3,31 @@ Capybara.default_max_wait_time = 5
 
 RSpec.feature "new tweet" do
 
-  let(:user) { FactoryGirl.create(:user) }
-
-  background do
-    login_as(user, scope: :user)
-  end
-
   context "Create a new tweet" do
     it "filling the fields and click submit" do
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
       visit '/'
       fill_in 'content_tweet', with: 'Its just a tweet'
       expect{ click_button 'Tweet' }.to change(Tweet, :count).by(+1)
+      expect{ find("#new_tweet").have_content 'Its just a tweet'}
+    end
+  end
+
+  context "Tweet of followed" do
+    it "filling the fields and click submit" do
+      user = FactoryGirl.create(:user)
+      user2 = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
+      visit user_path(user2)
+      find_link('Follow').click
+      logout(:user)
+      login_as(user2, scope: :user)
+      visit '/'
+      fill_in 'content_tweet', with: 'Its just a tweet'
+      click_button 'Tweet'
+      logout(:user)
+      login_as(user, scope: :user)
       expect{ find("#new_tweet").have_content 'Its just a tweet'}
     end
   end
