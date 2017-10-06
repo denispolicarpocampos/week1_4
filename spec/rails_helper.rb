54132f63f4b05fd1f3cdaafd374a1423c98a8f50
Daniel Moreto <dfmoreto@gmail.com>
2017-10-06 11:38:20 -0300

    Postgres extensions pg_trgm and fuzzystrmatch installed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,4 +20,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 end
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+end
+
 Capybara.javascript_driver = :poltergeist
